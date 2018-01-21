9f717d1a89c0bce3d062244ae49a6d425d42309d
adrisj7 <adrisj7@gmail.com>
2018-01-21 15:52:31 -0500

    Add FieldMeasurements, to be filled in and used in our Autons

---
+++ src/org/usfirst/frc/team694/robot/FieldMeasurements.java
@@ -0,0 +1,10 @@
+package org.usfirst.frc.team694.robot;
+
+/**
+ * FieldMeasurements contains the field measurements that we use in our autons.
+ * All of our autons should be based on the field so that if our measurements 
+ * change, we can adapt quickly
+ */
+public interface FieldMeasurements {
+
+}
