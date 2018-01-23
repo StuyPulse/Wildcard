1b9ea5c6bd54d4c1fd6e5aa881db25b617e108e2
Prog694 <prog694@gmail.com>
2018-01-22 19:59:35 -0500

    Add Drivetrain and PiotrDriveCommand

---
+++ src/org/usfirst/frc/team694/robot/commands/DrivetrainPiotrDriveCommand.java
@@ -0,0 +1,76 @@
+package org.usfirst.frc.team694.robot.commands;
+
+import org.usfirst.frc.team694.robot.Robot;
+
+import edu.wpi.first.wpilibj.command.Command;
+
+/**
+ *
+ */
+public class DrivetrainPiotrDriveCommand extends Command {
+
+    private boolean arcadeDrive;
+    private boolean wasPressed;
+
+    private double rightTrigger;
+    private double leftTrigger;
+
+    private double leftTriggerSquared;
+    private double rightTriggerSquared;
+
+    public DrivetrainPiotrDriveCommand() {
+        // Use requires() here to declare subsystem dependencies
+        requires(Robot.drivetrain);
+    }
+
+    // Called just before this Command runs the first time
+    @Override
+    protected void initialize() {
+        rightTriggerSquared = ((rightTrigger + 1) / 2) * ((rightTrigger + 1) / 2);
+        leftTriggerSquared = ((leftTrigger + 1) / 2) * ((leftTrigger + 1) / 2);
+    }
+
+    // Called repeatedly when this Command is scheduled to run
+    @Override
+    protected void execute() {
+        rightTrigger = Robot.oi.driverGamepad.getLeftTriggerAxis();
+        leftTrigger = Robot.oi.driverGamepad.getRightTriggerAxis();
+
+        rightTriggerSquared = ((rightTrigger + 1) / 2) * ((rightTrigger + 1) / 2);
+        leftTriggerSquared = ((leftTrigger + 1) / 2) * ((leftTrigger + 1) / 2);
+
+        if (Robot.oi.driverGamepad.getRawButton(1) && wasPressed == false) {
+            Robot.drivetrain.toggle();
+        }
+        wasPressed = Robot.oi.driverGamepad.getRawButton(1);
+        if (Robot.drivetrain.isToggled()) {
+            Robot.drivetrain.arcadeDrive((-1.0 * leftTriggerSquared) + rightTriggerSquared,
+                    -1 * (Math.signum(Robot.oi.driverGamepad.getLeftX()) * Robot.oi.driverGamepad.getLeftX()
+                            * Robot.oi.driverGamepad.getLeftX()));
+        } else {
+            Robot.drivetrain.tankDrive(
+                    -1.0 * Math.signum(Robot.oi.driverGamepad.getRightY()) * Robot.oi.driverGamepad.getRightY()
+                            * Robot.oi.driverGamepad.getRightY(),
+                    -1.0 * Math.signum(Robot.oi.driverGamepad.getLeftY()) * Robot.oi.driverGamepad.getLeftY()
+                            * Robot.oi.driverGamepad.getLeftY());
+        }
+    }
+
+    // Make this return true when this Command no longer needs to run execute()
+    @Override
+    protected boolean isFinished() {
+        return false;
+    }
+
+    // Called once after isFinished returns true
+    @Override
+    protected void end() {
+    }
+
+    // Called when another command which requires one or more of the same
+    // subsystems is scheduled to run
+    @Override
+    protected void interrupted() {
+    }
+
+}
---
+++ src/org/usfirst/frc/team694/robot/subsystems/Drivetrain.java
@@ -0,0 +1,129 @@
+/*----------------------------------------------------------------------------*/
+/* Copyright (c) 2017-2018 FIRST. All Rights Reserved.                        */
+/* Open Source Software - may be modified and shared by FRC teams. The code   */
+/* must be accompanied by the FIRST BSD license file in the root directory of */
+/* the project.                                                               */
+/*----------------------------------------------------------------------------*/
+
+package org.usfirst.frc.team694.robot.subsystems;
+
+import org.usfirst.frc.team694.robot.OI;
+
+import com.ctre.phoenix.motorcontrol.NeutralMode;
+import com.ctre.phoenix.motorcontrol.can.WPI_TalonSRX;
+
+import edu.wpi.first.wpilibj.Encoder;
+import edu.wpi.first.wpilibj.SpeedControllerGroup;
+import edu.wpi.first.wpilibj.command.Subsystem;
+import edu.wpi.first.wpilibj.drive.DifferentialDrive;
+
+/**
+ * An example subsystem. You can replace me with your own Subsystem.
+ */
+public class Drivetrain extends Subsystem {
+    private static WPI_TalonSRX leftFrontMotor;
+    private static WPI_TalonSRX leftRearMotor;
+    private static WPI_TalonSRX rightFrontMotor;
+    private static WPI_TalonSRX rightRearMotor;
+
+    private SpeedControllerGroup leftSpeedController;
+    private SpeedControllerGroup rightSpeedController;
+
+    public static OI oi;
+
+    private static DifferentialDrive differentialDrive;
+
+    private static Encoder leftEncoder;
+    private static Encoder rightEncoder;
+
+    public Drivetrain() {
+        //TODO: Remove magic numbers: Add in RobotMap
+        leftFrontMotor = new WPI_TalonSRX(1);
+        leftRearMotor = new WPI_TalonSRX(2);
+        leftSpeedController = new SpeedControllerGroup(leftFrontMotor, leftRearMotor);
+
+        rightFrontMotor = new WPI_TalonSRX(3);
+        rightRearMotor = new WPI_TalonSRX(4);
+        rightSpeedController = new SpeedControllerGroup(rightFrontMotor, rightRearMotor);
+
+        leftFrontMotor.setInverted(false);
+        leftRearMotor.setInverted(false);
+        rightFrontMotor.setInverted(false);
+        rightRearMotor.setInverted(false);
+
+        leftFrontMotor.setNeutralMode(NeutralMode.Brake);
+        leftRearMotor.setNeutralMode(NeutralMode.Brake);
+        rightFrontMotor.setNeutralMode(NeutralMode.Brake);
+        rightRearMotor.setNeutralMode(NeutralMode.Brake);
+
+        leftEncoder = new Encoder(0, 1);
+        rightEncoder = new Encoder(2, 3);
+
+        //leftEncoder.setDistancePerPulse(RobotMap.DRIVETRAIN_ENCODER_INCHES_PER_PULSE);
+        //rightEncoder.setDistancePerPulse(RobotMap.DRIVETRAIN_ENCODER_INCHES_PER_PULSE);
+
+        differentialDrive = new DifferentialDrive(leftSpeedController, rightSpeedController);
+
+        oi = new OI();
+
+    }
+
+    public static void resetEncoders() {
+        leftEncoder.reset();
+        rightEncoder.reset();
+    }
+
+    public static double getLeftSpeed() {
+        return leftEncoder.getRate();
+    }
+
+    public static double getRightSpeed() {
+        return rightEncoder.getRate();
+    }
+
+    public static double getSpeed() {
+        return Math.max(Math.abs(getLeftSpeed()), Math.abs(getRightSpeed()));
+    }
+
+    public static double getEncoderDistance() {
+        return Math.abs(Math.max(getLeftEncoderDistance(), getRightEncoderDistance()));
+    }
+
+    public static double getLeftEncoderDistance() {
+        return leftEncoder.getDistance();
+    }
+
+    public static double getRightEncoderDistance() {
+        return rightEncoder.getDistance();
+    }
+
+    public static void tankDrive(double left, double right) {
+        differentialDrive.tankDrive(left, right);
+    }
+
+    public static void arcadeDrive(double speed, double rotation) {
+        differentialDrive.arcadeDrive(speed, rotation);
+    }
+
+    public static void stop() {
+        tankDrive(0, 0);
+    }
+
+    /*TODO: Should we remove?
+    public static int leftEncoderRaw() {
+        return leftEncoder.getRaw();
+    }
+    
+    public static int rightEncoderRaw() {
+        return rightEncoder.getRaw();
+    }
+    
+    public static int encoderRaw() {
+        return Math.abs(Math.max(rightEncoderRaw(), leftEncoderRaw()));
+    }*/
+
+    public void initDefaultCommand() {
+        //setDefaultCommand(new DriveCommand());
+    }
+
+}
