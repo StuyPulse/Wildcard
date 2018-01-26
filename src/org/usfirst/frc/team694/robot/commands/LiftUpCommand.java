5764496f39211d9b8dddb9599b974a3773f2dff0
tahsinahmed13 <tahmed10@stuy.edu>
2018-01-26 16:39:34 -0500

    Update lift up and down commands to have interrupted cappibalities.

--- src/org/usfirst/frc/team694/robot/commands/LiftUpCommand.java
+++ src/org/usfirst/frc/team694/robot/commands/LiftUpCommand.java
@@ -32,11 +32,11 @@ public class LiftUpCommand extends Command {
 
     // Called once after isFinished returns true
     protected void end() {
-        Robot.lift.setBrakeOn();
     }
 
     // Called when another command which requires one or more of the same
     // subsystems is scheduled to run
     protected void interrupted() {
+        Robot.lift.setBrakeOn();
     }
 }
