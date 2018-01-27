2ce89a9d88676eb12654540ae25b61cc26d8eeac
vs2961 <siuleungjing@gmail.com>
2018-01-27 16:10:43 -0500

    Adding the values for Auton_Scale commands

--- src/org/usfirst/frc/team694/robot/commands/auton/DifferentSideScaleAutonCommand.java
+++ src/org/usfirst/frc/team694/robot/commands/auton/DifferentSideScaleAutonCommand.java
@@ -1,5 +1,6 @@
 package org.usfirst.frc.team694.robot.commands.auton;
 
+import org.usfirst.frc.team694.robot.FieldMap;
 import org.usfirst.frc.team694.robot.commands.DrivetrainRotateDegreesPIDCommand;
 import org.usfirst.frc.team694.robot.commands.GrabberOpenCommand;
 import org.usfirst.frc.team694.robot.commands.LiftMaxUpCommand;
@@ -9,22 +10,23 @@ import edu.wpi.first.wpilibj.command.CommandGroup;
 
 
 public class DifferentSideScaleAutonCommand extends CommandGroup {
+    public double speed = 0.5;
     public DifferentSideScaleAutonCommand() {
-        addSequential(new DrivetrainMoveToLineCommand(speed));
-        addSequential(new DrivetrainMoveInchesEncoderCommand(FirstLineToSpaceBetweenScaleAndSwitch, speed));
+        addSequential(new DrivetrainMoveToLineCommand(1, speed));
+        addSequential(new DrivetrainMoveInchesEncoderCommand(FieldMap.DISTANCE_TO_TRAVEL_BEFORE_FIRST_TURN_FROM_AUTO_LINE, speed));
         addSequential(new DrivetrainRotateDegreesPIDCommand(90));
-        addSequential(new DrivetrainMoveToLineCommand(speed));
+        addSequential(new DrivetrainMoveToLineCommand(2, speed));
         addParallel(new LiftMaxUpCommand());
         addSequential(new DrivetrainRotateDegreesPIDCommand(-90));
-        addSequential(new DrivetrainMoveToLineCommand(speed));
+        addSequential(new DrivetrainMoveToLineCommand(1, speed));
         addSequential(new GrabberOpenCommand());
-      
-            /*addSequential(new DrivetrainMoveToLineCommand(1, maxDistance, speed));
-            addSequential(new DrivetrainMoveInchesEncoderCommand(halfNullDistance, speed));
+        /*
+            addSequential(new DrivetrainMoveToLineCommand(1, speed));
+            addSequential(new DrivetrainMoveInchesEncoderCommand(FieldMap.DISTANCE_FROM_NULL_TERRITORY_TO_NULL_BUMP, speed));
             addParallel(new LiftMaxUpCommand());
             addSequential(new DrivetrainRotateDegreesPIDCommand(-90));
-            addSequential(new DrivetrainMoveInchesEncoderCommand(halfNullToScale, speed));
+            addSequential(new DrivetrainMoveInchesEncoderCommand(FieldMap.DISTANCE_FROM_BORDER_TO_SCALE_EDGE/2, speed));
             addSequential(new GrabberOpenCommand());
-            */
+       */     
     }
 }
--- src/org/usfirst/frc/team694/robot/commands/auton/SameSideScaleAutonCommand.java
+++ src/org/usfirst/frc/team694/robot/commands/auton/SameSideScaleAutonCommand.java
@@ -1,5 +1,6 @@
 package org.usfirst.frc.team694.robot.commands.auton;
 
+import org.usfirst.frc.team694.robot.FieldMap;
 import org.usfirst.frc.team694.robot.commands.DrivetrainRotateDegreesPIDCommand;
 import org.usfirst.frc.team694.robot.commands.GrabberOpenCommand;
 import org.usfirst.frc.team694.robot.commands.LiftMaxUpCommand;
@@ -10,13 +11,13 @@ import edu.wpi.first.wpilibj.command.CommandGroup;
  *
  */
 public class SameSideScaleAutonCommand extends CommandGroup {
-
+    public double speed = 0.5;
     public SameSideScaleAutonCommand() {
-        addSequential(new DrivetrainMoveToLineCommand(speed));
-        addSequential(new DrivetrainRotateDegreesPIDCommand(degrees));
-        addSequential(new DrivetrainMoveInchesEncoderCommand(hypothenuse, speed));
+        addSequential(new DrivetrainMoveToLineCommand(1, speed));
+        addSequential(new DrivetrainRotateDegreesPIDCommand(FieldMap.DEGREE_OF_ANGLE_TO_REACH_PLATFORM_ZONE_EDGE_FROM_AUTO_LINE));
+        addSequential(new DrivetrainMoveInchesEncoderCommand(FieldMap.DISTANCE_TO_TRAVEL_TO_REACH_PLATFORM_ZONE_EDGE, speed));
         addParallel(new LiftMaxUpCommand());
-        addSequential(new DrivetrainRotateDegreesPIDCommand(-degrees));
+        addSequential(new DrivetrainRotateDegreesPIDCommand(-FieldMap.DEGREE_OF_ANGLE_TO_REACH_PLATFORM_ZONE_EDGE_FROM_AUTO_LINE));
         addSequential(new GrabberOpenCommand());
     }
 }
