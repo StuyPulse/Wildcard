faa82d545f0cd6fa2c05f7f7c6ac6731505bd3c2
blai00 <blai00@stuy.edu>
2018-01-27 16:39:50 -0500

    Add FieldMap Reference Measurements to FieldMap.java

--- src/org/usfirst/frc/team694/robot/FieldMap.java
+++ src/org/usfirst/frc/team694/robot/FieldMap.java
@@ -7,24 +7,31 @@ package org.usfirst.frc.team694.robot;
  */
 public interface FieldMap {
 
-/* Given FRC Manual Measurements*/
+/* Field Map Reference Measurements*/
+    /* Link: https://docs.google.com/drawings/d/1DOinHilhxFRU5PtBTQs2ueSTpObqn2157AsnSt7vTJg/edit */
     
-    public static final int DISTANCE_FROM_ALLIANCE_STATION_TO_AUTO_LINE = 120;
-    public static final int DISTANCE_FROM_ALLIANCE_STATION_TO_SWITCH = 168;
-    public static final int STARTING_PLATE_HEIGHT_OF_SCALE = 60;
-    public static final int HIGHEST_PLATE_HEIGHT_OF_SCALE = 72;
-    public static final int LOWEST_PLATE_HEIGHT_OF_SCALE = 48;
-    public static final int HEIGHT_FROM_CARPET_TO_RUNG = 84;
-    public static final double HEIGHT_OF_SWITCH_FENCE = 18.75;
     
-    /*null territory --> 6 ft, 3ft*/
+    public static final int DISTANCE_FROM_ALLIANCE_STATION_TO_NULL_TERRITORY = 288;
+    public static final int DISTANCE_FROM_ALLANCE_STATION_TO_AUTO_LINE = 120;
+    public static final int DISTANCE_FROM_ALLIANCE_STATION_TO_SWITCH = 140;
+    public static final int DISTANCE_FROM_ALLIANCE_STATION_TO_FAR_SIDE_OF_SWITCH = 196;
+    public static final double DISTANCE_FROM_ALLIANCE_STATION_TO_PLATFORM_EDGE = 261.47;
+    public static final double DISTANCE_FROM_ALLIANCE_STATION_TO_SCALE_SIDE = 299.65;
+    public static final int DISTANCE_FROM_NULL_TERRITORY_TO_NULL_BUMP = 36;
+    public static final double DISTANCE_FROM_BORDER_TO_PLATFORM_ZONE_EDGE = 95.25;
+    public static final int DISTANCE_FROM_BORDER_TO_ROBOT_STARTING_POINT = 0;
+    public static final int DISTANCE_FROM_SCALE_SIDE_TO_NULL_TERRITORY = 0;
+    public static final double DISTANCE_FROM_FAR_SIDE_OF_SWITCH_TO_SCALE_SIDE = 103.65;
+    public static final int DISTANCE_FROM_NULL_TERRITORY_TO_AUTO_LINE = 168;
+    public static final int DISTANCE_FROM_ROBOT_STARTING_POINT_TO_PLATFORM_ZONE_EDGE = 0;
+    public static final int DISTANCE_TO_TRAVEL_TO_REACH_PLATFORM_ZONE_EDGE = 0;
+    public static final int DISTANCE_FROM_AUTO_LINE_TO_FAR_SIDE_OF_SWITCH = 76;
+    public static final double DISTANCE_FROM_FAR_SIDE_OF_SWITCH_TO_PLATFORM_EDGE = 65.47;
+    public static final int DISTANCE_TO_TRAVEL_BEFORE_FIRST_TURN_FROM_AUTO_LINE = 0;
+    public static final double DISTANCE_FROM_PLATFORM_ZONE_EDGE_TO_SCALE_EDGE = 23.68;
+    public static final int DISTANCE_TO_TRAVEL_TO_REACH_SCALE_SIDE = 0;
+    
 
-    /* Calculated Measurement Values */
-    
-    
-    /* Total Width - (2 * AutoLine and Alliance Station Distances) = 14ft. Divide 14ft by 2 to get 7ft.
-     * 7ft + 10ft = 17ft = 204in. */
-    public static final int DISTANCE_FROM_ALLIANCE_STATION_TO_SCALE = 204;
     
          
 }
