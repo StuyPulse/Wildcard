c996c79cabcf0402afa67ac5526b59fa957646fe
Brian Lin <16246866+BianoBonzoIII@users.noreply.github.com>
2018-01-27 12:13:22 -0500

    Update IRSensor

---
+++ src/org/usfirst/frc/team694/util/IRSensor.java
@@ -0,0 +1,54 @@
+package org.usfirst.frc.team694.util;
+
+import org.usfirst.frc.team694.robot.RobotMap;
+
+import edu.wpi.first.wpilibj.AnalogInput;
+import edu.wpi.first.wpilibj.Timer;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+
+public class IRSensor {
+    //Equation numbers
+    //Resulted from forming an equation with graph using many coordinates and calculator
+    //Power Regression used
+
+    private AnalogInput distanceSensor;
+
+    // Create instance of a timer that we can use to keep track of how long the
+    // gear is kept in the position for.
+    private Timer timeSinceEntry;
+    // We manually record whether the timer is running (Timer keeps that information private...)
+    private boolean isTimerRunning;
+
+    public IRSensor() {
+        distanceSensor = new AnalogInput(RobotMap.IR_SENSOR_PORT);
+        timeSinceEntry = new Timer();
+        isTimerRunning = false;
+    }
+
+    public double getDistance() {
+        return RobotMap.CONVERSION_FACTOR_CM_TO_INCHES
+                * (RobotMap.EQUATION_FACTOR * Math.pow(getVoltage(), RobotMap.EQUATION_EXPONENT));
+        // Link to Graph of Equation:
+        // http://image.dfrobot.com/image/data/SEN0143/GP2Y0A41SK%20datasheet.pdf (pg. 4)
+        // This method is an approximation of that graph.
+    }
+
+    public double getVoltage() {
+        return distanceSensor.getVoltage();
+    }
+
+    public boolean isCubeDetected() {
+        return getVoltage() > SmartDashboard.getNumber("IRVoltageThreshold", RobotMap.IR_SENSOR_THRESHOLD);
+    }
+
+    // The use of LEDs for the robot is currently unclear so this needs further detail in the future
+    /*
+    public void cubeLEDSignalControl() {
+        if (isCubeDetected()) {
+            Robot.ledCubeSensingSignal.stayOn();
+        } else {
+            Robot.ledCubeSensingSignal.stayOff();
+        }
+    }
+    */
+}
