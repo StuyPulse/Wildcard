2075dc1292e1d39329b84f7df3cffa43afe9d766
unknown <gliang90@stuy.edu>
2018-02-02 16:03:49 -0500

    Saved OI

--- src/org/usfirst/frc/team694/robot/OI.java
+++ src/org/usfirst/frc/team694/robot/OI.java
@@ -10,6 +10,7 @@ package org.usfirst.frc.team694.robot;
 import org.usfirst.frc.team694.util.Gamepad;
 import org.usfirst.frc.team694.util.Gamepad.GamepadSwitchMode;
 
+
 /**
  * This class is the glue that binds the controls on the physical operator
  * interface to the commands and command groups that allow control of the robot.
