3aef2d97a39a8a037fb9bda7e20e9789ad3feb0b
Pratham Rawat <prathamscience@gmail.com>
2018-02-03 10:35:36 -0500

    Address changes requested on the pull request.

--- src/org/usfirst/frc/team694/robot/commands/LiftDownCommand.java
+++ src/org/usfirst/frc/team694/robot/commands/LiftDownCommand.java
@@ -22,7 +22,7 @@ public class LiftDownCommand extends Command {
 
     // Called repeatedly when this Command is scheduled to run
     protected void execute() {
-        Robot.lift.setLiftMotors(-1);
+        Robot.lift.goDown();
     }
 
     // Make this return true when this Command no longer needs to run execute()
