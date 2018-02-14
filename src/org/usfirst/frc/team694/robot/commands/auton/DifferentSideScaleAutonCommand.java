dbce5b15084a32e6b9056f951ae3f7221ac81324
Pak Ming Lau <plau10@stuy.edu>
2018-02-14 17:18:37 -0500

    Fix auto commands to work with the new line sensor system and the new constants

--- src/org/usfirst/frc/team694/robot/commands/auton/DifferentSideScaleAutonCommand.java
+++ src/org/usfirst/frc/team694/robot/commands/auton/DifferentSideScaleAutonCommand.java
@@ -4,14 +4,16 @@ import org.usfirst.frc.team694.robot.FieldMapInterface;
 import org.usfirst.frc.team694.robot.Robot;
 import org.usfirst.frc.team694.robot.commands.DrivetrainRotateDegreesPIDCommand;
 import org.usfirst.frc.team694.robot.commands.GrabberOpenCommand;
+import org.usfirst.frc.team694.robot.commands.LiftMoveToHeightCommand;
 
 import edu.wpi.first.wpilibj.command.CommandGroup;
 
 public class DifferentSideScaleAutonCommand extends CommandGroup {
-    public static FieldMapInterface Quad = Robot.currentQuad;
+    public static FieldMapInterface Quad = Robot.getRobotQuadrant();
     public double speed = 0.5;
 
     public DifferentSideScaleAutonCommand() {  
+        /*
         addSequential(new DrivetrainMoveToLineCommand(speed, Quad.getDistanceFromLineSensorToAutoLine()));
         addSequential(new DrivetrainMoveInchesEncoderCommand(speed, Quad.getDistanceToTravelBeforeFirstTurn()));
         addSequential(new DrivetrainRotateDegreesPIDCommand(Quad.getDegreeOfAngleToTurnToReachPlatformZone()));
@@ -21,6 +23,19 @@ public class DifferentSideScaleAutonCommand extends CommandGroup {
         addSequential(new DrivetrainRotateDegreesPIDCommand(Quad.getDegreeOfAngleToTurnToReachScaleSide()));
 
         addSequential(new DrivetrainMoveToLineCommand(speed, Quad.getDistanceToTravelToReachScaleSide()));
+        addSequential(new GrabberOpenCommand());
+        */
+        
+        addSequential(new DrivetrainMoveInchesEncoderCommand(speed, Quad.getTotalDistanceToTravelBeforeTurn()));
+        //add line system code using this value: Quad.getDistanceFromLineSensorToAutoLine()
+        addSequential(new DrivetrainRotateDegreesPIDCommand(Quad.getDegreeOfAngleToTurnToReachPlatformZone()));
+        addSequential(new DrivetrainMoveInchesEncoderCommand(speed, Quad.getTotalDistanceToTravelToReachOtherSideOfPlatformZone()));
+        //add line system code using this value: Quad.getDistanceToTravelToReachPlatformZone()
+        //add line system code using this value: Quad.getDistanceFromStartingPointToOtherPlatformZoneEdge()
+        addSequential(new DrivetrainRotateDegreesPIDCommand(Quad.getDegreeOfAngletoTurnToReachScaleSide()));
+        addSequential(new LiftMoveToHeightCommand(84));//unsure about height
+        addSequential(new DrivetrainMoveInchesEncoderCommand(speed, Quad.getTotalDistanceToTravelToReachScaleSide()));
+        //add line system code using this value: Quad.getDistanceFromRobotAfterTwoTurnsToNullTerritory()
         addSequential(new GrabberOpenCommand());
     }
 }
--- src/org/usfirst/frc/team694/robot/commands/auton/SameSideScaleAutonCommand.java
+++ src/org/usfirst/frc/team694/robot/commands/auton/SameSideScaleAutonCommand.java
@@ -2,6 +2,7 @@ package org.usfirst.frc.team694.robot.commands.auton;
 
 import org.usfirst.frc.team694.robot.FieldMapInterface;
 import org.usfirst.frc.team694.robot.Robot;
+import org.usfirst.frc.team694.robot.commands.DrivetrainRotateDegreesPIDCommand;
 import org.usfirst.frc.team694.robot.commands.GrabberOpenCommand;
 import org.usfirst.frc.team694.robot.commands.LiftMoveToHeightCommand;
 
@@ -9,13 +10,12 @@ import edu.wpi.first.wpilibj.command.CommandGroup;
 
 public class SameSideScaleAutonCommand extends CommandGroup {
     public double speed = 0.5;
-    public static FieldMapInterface Quad = Robot.currentQuad;
+    public static FieldMapInterface Quad = Robot.getRobotQuadrant();
     public SameSideScaleAutonCommand() {
-        addSequential(new DrivetrainMoveToLineCommand(speed, Quad.getDistanceFromLineSensorToAutoLine()));
-        addSequential(new DrivetrainMoveToLineCommand(speed, Quad.getDistanceFromAutoLineToNullTerritory()));
-        addSequential(new DrivetrainMoveInchesEncoderCommand(-1 * speed, Quad.getDistanceToMoveBackwardBeforeTurn()));
-        //addSequential(new DrivetrainRotateDegreesPIDCommand(Quad.getDegreeOfAngleToReachIdealStartingPointFromNullTerritoryLine()));
-        addSequential(new LiftMoveToHeightCommand(84)); //Unsure about this height
+        addSequential(new DrivetrainMoveInchesEncoderCommand(speed, Quad.getTotalDistanceToTravelToReachNullLine()));
+        //add line system code using this value: Quad.getDistanceFromLineSensorToAutoLine()
+        addSequential(new DrivetrainRotateDegreesPIDCommand(Quad.getDegreeOfAngleToReachIdealStartingPointFromNullTerritoryLine()));
+        addSequential(new LiftMoveToHeightCommand(84)); //unsure about height
         addSequential(new DrivetrainMoveInchesEncoderCommand(speed, Quad.getDistanceToTravelToReachScaleCorner()));
         addSequential(new GrabberOpenCommand());
     }
