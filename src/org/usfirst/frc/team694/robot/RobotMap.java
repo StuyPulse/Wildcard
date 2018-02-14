377041658de86339157bb862338878c2120d1c69
Justin Time <prog694@gmail.com>
2018-02-14 16:35:51 -0500

    Merged RobotMap from testing into master using new method, might not be perfect

--- src/org/usfirst/frc/team694/robot/RobotMap.java
+++ src/org/usfirst/frc/team694/robot/RobotMap.java
@@ -14,9 +14,6 @@ public final class RobotMap {
     public static final double DISTANCE_LINE_SENSOR_IS_FROM_FRONT_BUMPER_OF_BOT = 3.75; //
     public static final double LENGTH_OF_BOT = 39.5; // (not including grabber)
     public static final double WIDTH_OF_BOT = 34.5; //
-    public static final double MIDDLE_OF_BOT_LENGTHWISE = LENGTH_OF_BOT/2;
-    public static final double MIDDLE_OF_BOT_WIDTHWISE = WIDTH_OF_BOT/2;
-    public static final double DIFFERENCE_BETWEEN_LENGTH_OF_ROBOT_AND_WIDTH_OF_ROBOT_AFTER_TURN = (LENGTH_OF_BOT - WIDTH_OF_BOT) / 2;
     /**************************************************************************
      * Gamepad Ports
      *************************************************************************/
@@ -29,17 +26,17 @@ public final class RobotMap {
     public static final int DRIVETRAIN_LEFT_TOP_MOTOR_PORT = 3;
     public static final int DRIVETRAIN_LEFT_MIDDLE_MOTOR_PORT = 2;
     public static final int DRIVETRAIN_LEFT_BOTTOM_MOTOR_PORT = 1;
-    
+
+    /**************************************************************************
+     * Spatula and CrabArm Constants //All times are in seconds
+     *************************************************************************/
+    public static final double PRE_FLIP_WAIT_TIME = 2;
+    public static final double POST_FLIP_WAIT_TIME = 2;
+    public static final double FLAP_OUT_SPEED = 1;
 
     public static final int DRIVETRAIN_RIGHT_TOP_MOTOR_PORT = 6;
     public static final int DRIVETRAIN_RIGHT_MIDDLE_MOTOR_PORT = 5;
     public static final int DRIVETRAIN_RIGHT_BOTTOM_MOTOR_PORT = 4;
-    /**************************************************************************
-     * Spatula and CrabArm Constants         //All times are in seconds
-     *************************************************************************/
-    public static final double PRE_FLIP_WAIT_TIME = 2;
-    public static final double POST_FLIP_WAIT_TIME = 2;
-    public static final double FLAP_OUT_SPEED = 0.5;
 
     /**************************************************************************
      * Spatula Motor Ports
@@ -50,8 +47,8 @@ public final class RobotMap {
     /**************************************************************************
      * Spatula Limit Switch
      *************************************************************************/
-    public static final int SPATULA_LIMIT_SWITCH_PORT = 0;
-    
+    public static final int SPATULA_LIMIT_SWITCH_PORT = 1;
+
     /***************************************************************************
      * Lift Motor Ports
      *************************************************************************/
@@ -63,7 +60,7 @@ public final class RobotMap {
     /***************************************************************************
      * Lift Constants
      *************************************************************************/
-    public static final double LIFT_DIAMETER_OF_ENCODER_SPROCKET = 2.873; 
+    public static final double LIFT_DIAMETER_OF_ENCODER_SPROCKET = 2.873;
     public static final double LIFT_TOTAL_CARRIAGE_MOVEMENT = 92.25;
     public static final double LIFT_ENCODER_RAW_MULTIPLIER = LIFT_DIAMETER_OF_ENCODER_SPROCKET * Math.PI * 3 / 1024;
     public static final double MIN_HEIGHT_OF_LIFT = 23;
@@ -74,30 +71,31 @@ public final class RobotMap {
     public static final double LIFT_HEIGHT_THRESHOLD = 20.0;
     public static final double LIFT_RAMP_SLOPE = (LIFT_MAX_SPEED - LIFT_MIN_SPEED) / LIFT_HEIGHT_THRESHOLD;
     public static final double LIFT_HEIGHT_TOLERANCE = 1.0;
-    
+
     /***************************************************************************
      * Lift Limit Switch
      *************************************************************************/
     public static final int LIFT_TOP_LIMIT_SWITCH_PORT = -1;
-    public static final int LIFT_BOTTOM_LIMIT_SWITCH_PORT = -1;
-    
+    public static final int LIFT_BOTTOM_LIMIT_SWITCH_PORT = 0;
+
     /****************************************************************************
      * CrabArm Ports
      *************************************************************************/
-    
+
     public static final int CRAB_ARM_LEFT_MOTOR_PORT = 13;
     public static final int CRAB_ARM_RIGHT_MOTOR_PORT = 14;
-    
+
     /****************************************************************************
      * Solenoid Ports
      *************************************************************************/
-    public static final int GEAR_SHIFT_CHANNEL = -1;
+    public static final int GEAR_SHIFT_CHANNEL = 7;
 
-    public static final int GRABBER_SOLENOID_OPEN_PORT = -1;
-    public static final int GRABBER_SOLENOID_CLOSE_PORT = -1;
+    public static final int GRABBER_SOLENOID_OPEN_PORT = 2;
+    public static final int GRABBER_SOLENOID_CLOSE_PORT = 3;
 
-    public static final int SPATULA_FLIP_SOLENOID_PORT = -1;
-    public static final int SPATULA_TONGS_SOLENOID_PORT = -1; 
+    public static final int SPATULA_FLIP_UP_PORT = 6;
+    public static final int SPATULA_FLIP_DOWN_PORT = 5;
+    //    public static final int SPATULA_TONGS_SOLENOID_PORT = -1; 
 
     public static final int LIFT_BRAKE_SOLENOID_PORT = -1;
 
@@ -115,9 +113,13 @@ public final class RobotMap {
     public static final int DRIVETRAIN_ENCODERS_PULSES_PER_REVOLUTION = 256;
     public static final double DRIVETRAIN_ENCODERS_FACTOR = 4.0;
     public static final double DRIVETRAIN_ENCODERS_INCHES_PER_REVOLUTION = Math.PI * DRIVETRAIN_WHEEL_DIAMETER;
-    
-    /** DRIVETRAIN_RAW_MULTIPLIER: We multiply by 4 because the encoder has 4 Quadrants, and each Quadrant passes 256 pulses. **/
-    public static final double DRIVETRAIN_RAW_MULTIPLIER = DRIVETRAIN_ENCODERS_INCHES_PER_REVOLUTION / DRIVETRAIN_ENCODERS_PULSES_PER_REVOLUTION;
-    
+
+    /**
+     * DRIVETRAIN_RAW_MULTIPLIER: We multiply by 4 because the encoder has 4
+     * Quadrants, and each Quadrant passes 256 pulses.
+     **/
+    public static final double DRIVETRAIN_RAW_MULTIPLIER = 
+            DRIVETRAIN_ENCODERS_INCHES_PER_REVOLUTION
+            / (DRIVETRAIN_ENCODERS_FACTOR * DRIVETRAIN_ENCODERS_PULSES_PER_REVOLUTION);
 
 }
