6e0ca18d2ba26750b59b736d4040308e11a5bf01
Pratham Rawat <prathamscience@gmail.com>
2018-02-14 19:18:36 -0500

    Fix the Move method to incorportate full joystick control and squared inputs

--- src/org/usfirst/frc/team694/robot/commands/LiftMoveCommand.java
+++ src/org/usfirst/frc/team694/robot/commands/LiftMoveCommand.java
@@ -3,29 +3,20 @@ package org.usfirst.frc.team694.robot.commands;
 import org.usfirst.frc.team694.robot.Robot;
 
 import edu.wpi.first.wpilibj.command.Command;
-import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 /**
  *
  */
 public class LiftMoveCommand extends Command {
-    double power = 1; //TODO: Figure out what power should be set to
+    
     public LiftMoveCommand() {
         requires(Robot.lift);
     }
 
     protected void execute() {
-        double liftControlL = Robot.oi.operatorGamepad.getLeftY();
-
-        double liftSet = SmartDashboard.getNumber("Lift Operator setting.", power);
-
-        if(SmartDashboard.getNumber("Lift Operator setting.", power) % 2 == 0){
-          Robot.lift.move(Math.pow(liftControlL,liftSet) * Math.signum(liftControlL));
-        } else {
-          Robot.lift.move(Math.pow(liftControlL,liftSet));
-        }
-
-
-        SmartDashboard.getNumber("Lift Operator setting", power);
+        double liftControl = Robot.oi.operatorGamepad.getLeftY();
+        
+        Robot.lift.move(Math.pow(liftControl, 2) * Math.signum(liftControl));
+        //TODO: see whether Coby wants cubed or squared inputs on the lift.
     }
 
     protected boolean isFinished() {
@@ -33,6 +24,5 @@ public class LiftMoveCommand extends Command {
     }
 
     protected void end() {
-        Robot.lift.stop();
     }
 }
