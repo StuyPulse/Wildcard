0f15851004a9af2403c9bd728ecbfa3071a12c83
huiminwu0 <huiminwu24@gmail.com>
2018-02-16 13:11:24 -0500

    Add extra procedure to BITCOIN: Let the cube go up in the lift

--- src/org/usfirst/frc/team694/robot/commands/BITCOINCommand.java
+++ src/org/usfirst/frc/team694/robot/commands/BITCOINCommand.java
@@ -3,10 +3,18 @@ import edu.wpi.first.wpilibj.command.CommandGroup;
 
 public class BITCOINCommand extends CommandGroup {
 
+    // Once we get the cube in the grabber, 
+    private static final double SECONDS_TO_MOVE_CUBE_UP_AFTER_GRABBING = 0.6;
+
     public BITCOINCommand() {
+        // Get the cube in the grabber
         addSequential(new LiftMoveToHeightCommand(0));
         addSequential(new FlapAndFlipUpCommand());
         addSequential(new GrabberCloseCommand());
+
+        // Move the cube up a little to get it out of the spatula
+        addSequential(new SpatulaDeacquireCommand(), SECONDS_TO_MOVE_CUBE_UP_AFTER_GRABBING);
+        addParallel(new LiftMoveSpeedCommand(-0.7), SECONDS_TO_MOVE_CUBE_UP_AFTER_GRABBING);
         }
     
     public boolean isFinished() {
