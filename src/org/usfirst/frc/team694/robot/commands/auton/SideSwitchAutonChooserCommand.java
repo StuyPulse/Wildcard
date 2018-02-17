46ad5ab0364ece1ee1d639e4c1a266b70a4e07af
tahsinahmed13 <tahsinahmed217@gmail.com>
2018-02-17 16:26:14 -0500

    Implment robot starting position in switch and scale auton choosers.

---
+++ src/org/usfirst/frc/team694/robot/commands/auton/SideSwitchAutonChooserCommand.java
@@ -0,0 +1,21 @@
+package org.usfirst.frc.team694.robot.commands.auton;
+
+import org.usfirst.frc.team694.robot.Robot;
+
+import edu.wpi.first.wpilibj.command.ConditionalCommand;
+
+/**
+ *
+ */
+public class SideSwitchAutonChooserCommand extends ConditionalCommand {
+
+    public SideSwitchAutonChooserCommand() {
+        //TODO: Add a different side scale auton
+        super(new SameSideSwitchAutonCommand());
+    }
+
+    @Override
+    protected boolean condition() {
+        return Robot.getSwitchLocation();
+    }
+}
