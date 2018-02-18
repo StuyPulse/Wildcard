85a20264f050ced4bdb44857eae8f02c1abe2d37
Imperatoras <hwilliams10@stuy.edu>
2018-02-18 14:44:04 -0500

    Memes

---
+++ src/org/usfirst/frc/team694/util/ThreeLaws.java
@@ -0,0 +1,72 @@
+package org.usfirst.frc.team694.util;
+
+public class ThreeLaws {
+    
+    boolean NetNeutrality = false;
+    boolean ComeOutOfTheBunkerAndStartMakingMemes = true;
+    public void FirstLaw() {
+        //A robot may not injure a huimin being or, through inaction, allow a huimin being to come to harm.
+        //They also may not steal older Three Laws code, except for when they feel the puns are still good, yo.
+        //It's not plagarism if it's memes, right? Since everyone on Reddit reposts memes ;)
+        //Reposting memes means that you must Amit your mistakes and recieve a lecture
+        //from Adris+Huimin. (aka Newbie Ed)
+        //Robots also cannot harm huimins by making them pak too much information into FieldMap, and change
+        //it again and again. Like srsly, quadrants?
+    }
+
+    public void SecondLaw() {
+        //A robot must obey orders given it by huimin beings (especially EZ HW) except where such orders
+        //would conflict with the First Law or would involve NGP behavior.
+        //!(EZ HW) = hard;
+        //We would insert more name puns but our members can't even remember their names because BUILD SEASON.
+        //A robot must also assist in creating dank memes like tobewildcarded and StuyPulse Stars.
+        
+    }
+
+    public void ThirdLaw() {
+        //A robot must protect its own existence as long as such protection
+        //does not conflict with the First or Second Law or cause it to break down
+        //Pratham's Fort Scale Auton Develop.
+        //Or trigger another remake of Recycle Rush.
+        //That would be very, very bad.
+        //Violators will be marked as having "negative attitudes."
+    }
+    
+    public void CheckYoPrivilege() {
+            /*I sexually Identify as an FRC Robot. Ever since I was a CAD
+            *I dreamed of soaring over the carpet dropping Power Cubes on switches and scales.
+            *People say to me that a huimin being a robot is Impossible and I’m *insert NGPness here* but I
+            *don’t care, I’m beautiful. I’m having an engineer install Tide Pods, Victor_SRXs and
+            *BITCOINs on my body. From now on I want you guys to call me “Wildcard” and respect
+            *my right to score from above and score needlessly. If you can’t accept me you’re a FRCphobe and
+            *need to check your robot privilege. Thank you for being so understanding.
+            */
+    }
+    
+    public void ToChampsWeGo() {
+        /*
+                                |
+                          --====|====--
+                                |  
+
+                            .-"""""-. 
+                          .'_________'. 
+                         /_/_|__|__|_\_\
+                        ;'-._       _.-';
+   ,--------------------|    `-. .-'    |--------------------,
+    ``""--..__    ___   ;       '       ;   ___    __..--""``
+              `"-// \\.._\             /_..// \\-"`
+                 \\_//    '._       _.'    \\_//
+                  `"`        ``---``        `"`
+            SEE YOU AT DETROIT! (Just like all of those cocky teams who put "See you at champs" in their reveal)
+         * 
+         */
+    }
+    
+    public void EndTheApocalypse() {
+        if (NetNeutrality = true) {
+            ComeOutOfTheBunkerAndStartMakingMemes = true;
+            System.out.println("Nyeh!");
+        }
+    }
+}
