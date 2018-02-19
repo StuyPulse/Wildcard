7c3a49148106a46ff62c904d3a888fbbe311fff0
Pratham Rawat <prathamscience@gmail.com>
2018-02-19 11:22:25 -0500

    Change OI to use whenPressed in accordance with previous changes, and properly implement BITCOIN check functionality if isBITCOINAutomation is false.

--- src/org/usfirst/frc/team694/robot/commands/BITCOINCheckCommand.java
+++ src/org/usfirst/frc/team694/robot/commands/BITCOINCheckCommand.java
@@ -7,12 +7,12 @@ import edu.wpi.first.wpilibj.command.ConditionalCommand;
 public class BITCOINCheckCommand extends ConditionalCommand { 
     
     public BITCOINCheckCommand() {  
-        super(new BITCOINCommand(), new CrabArrowAcquireUntilAcquiredCommand());
+        super(new BITCOINCubeDetectedCommand(), new CrabArrowAcquireCommand());
     }
 
     @Override
     protected boolean condition() {
-        return Robot.spatula.isBITCOINAutomation && Robot.spatula.isCubeDetected();
+        return Robot.spatula.isBITCOINAutomation;
         // if true, will run BITCOINAutoCommand, which includes the acquirer limit switch
         // if false, will run the BITCOINManualCommand, in which the acquirer has already stopped
     }
