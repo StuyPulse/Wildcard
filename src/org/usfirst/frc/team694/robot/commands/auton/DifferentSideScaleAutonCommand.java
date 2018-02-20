5f3fcd2e252e4bbfa482405054695d5bdb44610b
tahsinahmed13 <tahsinahmed13@gmail.com>
2018-02-20 13:47:57 -0400

    Clean different side scale auton just a little

--- src/org/usfirst/frc/team694/robot/commands/auton/DifferentSideScaleAutonCommand.java
+++ src/org/usfirst/frc/team694/robot/commands/auton/DifferentSideScaleAutonCommand.java
@@ -32,41 +32,6 @@ public class DifferentSideScaleAutonCommand extends CommandGroup {
 
         addSequential(new GrabberOpenCommand());
 
-        
-        //addParallel(new DrivetrainLineSensorCommand(quad.getDistanceFromLineSensorToAutoLine()));
-        /*addParallel(new DrivetrainRampSwerveCommand(
-                quad.getTotalDistanceToTravelBeforeTurn() + quad.getTotalDistanceToTravelToReachOtherSideOfPlatformZone() + 15,
-                quad.getTotalDistanceToTravelBeforeTurn(),
-                -90
-                ));
-        */
-//        addSequential(new DrivetrainRampSwerveCommand(
-//            CURVE_1_DISTANCE_TOTAL,
-//            CURVE_1_DISTANCE_TILL_TURN,
-//            -100
-//        ));
-
-        /*
-        addSequential(new DriveStraightWithRampingCommand(quad.getTotalDistanceToTravelBeforeTurn() + 3));
-         
-
-        addSequential(new DrivetrainRotateDegreesPIDCommand(-1 * quad.getAngleToTurnToReachPlatformZone()));
-
-        //addParallel(new DrivetrainLineSensorPlatformZoneCommand());
-        addSequential(new DriveStraightWithRampingCommand(quad.getTotalDistanceToTravelToReachOtherSideOfPlatformZone() + 15));
-        */
-
-        /*
-        addSequential(new DrivetrainRotateDegreesPIDCommand(-1 * quad.getAngleToTurnToReachScaleSide()));
-
-        addSequential(new LiftMoveToHeightCommand(89 - RobotMap.MIN_HEIGHT_OF_LIFT));//unsure about height
-
-        //addParallel(new DrivetrainLineSensorCommand(quad.getDistanceFromRobotAfterTwoTurnsToNullTerritory()));
-        addSequential(new DriveStraightWithRampingCommand(53));//quad.getTotalDistanceToTravelToReachScaleSide() - 5));
-
-        addSequential(new GrabberOpenCommand());
-        */
-
     }
 
 }
