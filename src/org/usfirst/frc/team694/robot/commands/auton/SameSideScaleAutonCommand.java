db3e266a7faeb68cd0f813021b50961d9885f435
tahsinahmed13 <tahsinahmed217@gmail.com>
2018-02-28 16:42:44 -0400

    Fix BITCOIN such that the crab arms do not flap out when spatula flip up.

--- src/org/usfirst/frc/team694/robot/commands/auton/SameSideScaleAutonCommand.java
+++ src/org/usfirst/frc/team694/robot/commands/auton/SameSideScaleAutonCommand.java
@@ -2,6 +2,7 @@ package org.usfirst.frc.team694.robot.commands.auton;
 
 import org.usfirst.frc.team694.robot.RobotMap;
 import org.usfirst.frc.team694.robot.commands.GrabberOpenCommand;
+import org.usfirst.frc.team694.robot.commands.LiftMoveToBottomCommand;
 import org.usfirst.frc.team694.robot.commands.LiftMoveToHeightCommand;
 
 import edu.wpi.first.wpilibj.command.CommandGroup;
@@ -28,14 +29,17 @@ public class SameSideScaleAutonCommand extends CommandGroup {
 
         addSequential(new GrabberOpenCommand());
         addSequential(new DrivetrainMoveInchesEncoderCommand(-0.5, 10));
-//        addSequential(new DrivetrainRotateDegreesPIDCommand(-165));
+        addSequential(new LiftMoveToBottomCommand());
+
+        //        addSequential(new DrivetrainRotateDegreesPIDCommand(-165));
 
 //        addParallel(new CrabArmAcquireCommand());
-//        addParallel(new LiftMoveToBottomCommand());
 //        addSequential(new DrivetrainMoveInchesEncoderCommand(0.5,10));
 
-        addSequential(new ScaleGrabCubeAfterScoringCommand(true));
-        addSequential(new ScaleScoreSecondTimeCommand(true));
+//        addSequential(new ScaleGrabCubeAfterScoringCommand(true));
+//        addSequential(new ScaleScoreSecondTimeCommand(true));
+
+        
         // Move lift when we're kinda close
 //        addParallel(new ConditionalDistanceEncodersCommand(new LiftMoveToHeightCommand(89 - RobotMap.MIN_HEIGHT_OF_LIFT), DISTANCE_TOTAL - 100));
 //        addSequential(new DrivetrainRampSwerveCommand(DISTANCE_TOTAL, DISTANCE_TO_SWERVE, -30));
