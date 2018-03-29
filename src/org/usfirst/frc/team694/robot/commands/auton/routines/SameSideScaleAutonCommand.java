cbf9b30a67ecbc03b210dc51d19d26b16c18fa64
Renee <rmui10@stuy.edu>
2018-03-29 17:34:49 -0400

    Remove ternary operator.

--- src/org/usfirst/frc/team694/robot/commands/auton/routines/SameSideScaleAutonCommand.java
+++ src/org/usfirst/frc/team694/robot/commands/auton/routines/SameSideScaleAutonCommand.java
@@ -12,12 +12,11 @@ import org.usfirst.frc.team694.robot.commands.auton.DrivetrainDriveCurveCommand;
  */
 public class SameSideScaleAutonCommand extends AutonCommandGroup {
     private static final double TOTAL_DISTANCE = 286; //TODO: Not sure about this distance
-    boolean isRobotOnRight = Robot.isRobotStartingOnRight();
 
     public SameSideScaleAutonCommand() {
         DrivetrainDriveCurveCommand driveCommand = new DrivetrainDriveCurveCommand(TOTAL_DISTANCE);
         driveCommand.addSpeedChange(0, 0.8);
-        driveCommand.addTurn(261, isRobotOnRight ? 45:-45);
+        driveCommand.addTurn(261, 45);
         
         addParallel(new ConditionalDistanceEncodersCommand(new LiftMoveToHeightCommand(82.0), 168));
         addSequential(driveCommand, 3.5);
