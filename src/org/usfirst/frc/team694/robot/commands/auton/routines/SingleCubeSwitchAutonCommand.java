c2c8717dfe2ebc977b9e879539208b1003d17637
Adris Jautakas <adrisj7@gmail.com>
2018-04-02 11:41:29 -0400

    Working switch v2: Works on right side consistenly

--- src/org/usfirst/frc/team694/robot/commands/auton/routines/SingleCubeSwitchAutonCommand.java
+++ src/org/usfirst/frc/team694/robot/commands/auton/routines/SingleCubeSwitchAutonCommand.java
@@ -9,19 +9,23 @@ import org.usfirst.frc.team694.robot.commands.auton.DrivetrainMoveInchesEncoderC
 import org.usfirst.frc.team694.robot.commands.auton.DrivetrainDriveCurveCommand.RampMode;
 
 import edu.wpi.first.wpilibj.command.CommandGroup;
+import edu.wpi.first.wpilibj.command.WaitCommand;
 
 /**
  *
  */
 public class SingleCubeSwitchAutonCommand extends CommandGroup {
-
+    
     public SingleCubeSwitchAutonCommand() {
-        DrivetrainDriveCurveCommand driveCommand = new DrivetrainDriveCurveCommand(124);
-        driveCommand.addSpeedChange(0.0, 0.6);
-        driveCommand.addTurn(5.0, 90.0); // Originally was 10
-        driveCommand.addTurn(42.0 + 20, 0.0);
+        DrivetrainDriveCurveCommand driveCommand = new DrivetrainDriveCurveCommand(118);
+        driveCommand.addSpeedChange(0, 0.6);
+//        driveCommand.addSpeedChange(0.0, 0.4);
+//        driveCommand.addSpeedChange(0.0 + 10, 0.6);
+        driveCommand.addTurn(5.0, 90.0); // Originally was 10 inches
+        driveCommand.addTurn(42.0 + 20 + 6, 0.0);
 
         addSequential(new LiftMoveToHeightCommand(30.0)); //TODO: Find a height for the lift to move to.
+        addSequential(new WaitCommand(0.5));
         addSequential(driveCommand, 7);
         addSequential(new QuisitorDeacquireCommand());
         
