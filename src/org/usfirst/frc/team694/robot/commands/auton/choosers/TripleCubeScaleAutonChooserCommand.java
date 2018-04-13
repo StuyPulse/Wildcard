beb979456445791558c839075ae6f6bb151ec822
Winston Peng <wpeng00@stuy.edu>
2018-04-13 16:23:47 -0400

    add auton chooser for our ideal autons (3 cube same side scale and 2 cube opposite side scale)

--- src/org/usfirst/frc/team694/robot/commands/auton/choosers/TripleCubeScaleAutonChooserCommand.java
+++ src/org/usfirst/frc/team694/robot/commands/auton/choosers/TripleCubeScaleAutonChooserCommand.java
@@ -11,11 +11,36 @@ import edu.wpi.first.wpilibj.command.ConditionalCommand;
 public class TripleCubeScaleAutonChooserCommand extends ConditionalCommand {
 
     public TripleCubeScaleAutonChooserCommand() {
-        super(new TripleCubeScaleAutonCommand(true), new TripleCubeScaleAutonCommand(false));
+        super(new TripleCubeSameSideScaleAutonChooserCommand(), new DoubleCubeDifferentSideScale90DegreesAutonChooserCommand());
+        //super(new TripleCubeScaleAutonCommand(true), new TripleCubeScaleAutonCommand(false));
     }
 
     @Override
     public boolean condition() {
-        return Robot.isRobotStartingOnRight();
+        return Robot.isRobotOnSameSideScale();
+    }
+
+    private static class TripleCubeSameSideScaleAutonChooserCommand extends ConditionalCommand {
+
+        public TripleCubeSameSideScaleAutonChooserCommand() {
+            super(new TripleCubeScaleAutonCommand(true), new TripleCubeScaleAutonCommand(false));
+        }
+
+        @Override
+        public boolean condition() {
+            return Robot.isRobotStartingOnRight();
+        }
+    }
+
+    private static class DoubleCubeDifferentSideScale90DegreesAutonChooserCommand extends ConditionalCommand {
+
+        public DoubleCubeDifferentSideScale90DegreesAutonChooserCommand() {
+            super(new DoubleCubeDifferentSideScale90DegreesAutonCommand(true), new DoubleCubeDifferentSideScale90DegreesAutonCommand(false));
+        }
+
+        @Override
+        public boolean condition() {
+            return Robot.isRobotStartingOnRight();
+        }
     }
 }
