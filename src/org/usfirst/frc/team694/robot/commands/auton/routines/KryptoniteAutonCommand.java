13d45455ee25df58f016aac4760db935c228f431
Renee <rmui10@stuy.edu>
2018-04-14 15:55:37 -0400

    Add KryptoniteAutonCommand.

---
+++ src/org/usfirst/frc/team694/robot/commands/auton/routines/KryptoniteAutonCommand.java
@@ -0,0 +1,57 @@
+package org.usfirst.frc.team694.robot.commands.auton.routines;
+
+import org.usfirst.frc.team694.robot.commands.LiftMoveToBottomCommand;
+import org.usfirst.frc.team694.robot.commands.LiftMoveToHeightCommand;
+import org.usfirst.frc.team694.robot.commands.QuisitorAcquireCommand;
+import org.usfirst.frc.team694.robot.commands.QuisitorCloseCommand;
+import org.usfirst.frc.team694.robot.commands.QuisitorDeacquireCommand;
+import org.usfirst.frc.team694.robot.commands.QuisitorOpenCommand;
+import org.usfirst.frc.team694.robot.commands.auton.ConditionalDistanceEncodersCommand;
+import org.usfirst.frc.team694.robot.commands.auton.DriveStraightRampDownOnlyCommand;
+import org.usfirst.frc.team694.robot.commands.auton.DrivetrainMoveInchesEncoderCommand;
+import org.usfirst.frc.team694.robot.commands.auton.DrivetrainRotateAbsoluteDegreesPIDCommand;
+
+import edu.wpi.first.wpilibj.command.CommandGroup;
+
+/**
+ *
+ */
+public class KryptoniteAutonCommand extends CommandGroup {
+
+    public KryptoniteAutonCommand(boolean isRight) {
+        addParallel(new ConditionalDistanceEncodersCommand(new LiftMoveToHeightCommand(15.0), 10));
+        //TODO: Check distances
+        addSequential(new DriveStraightRampDownOnlyCommand(176));
+        addSequential(new DrivetrainRotateAbsoluteDegreesPIDCommand(isRight? -90 : 90));
+        //TODO: Check distance to move 
+        addSequential(new DrivetrainMoveInchesEncoderCommand(15, 0.5));
+        addSequential(new QuisitorDeacquireCommand());
+        
+        //To acquire and score second cube
+        addSequential(new DrivetrainMoveInchesEncoderCommand(15.0, -0.8));
+        addSequential(new DrivetrainRotateAbsoluteDegreesPIDCommand(180));
+        //TODO: Check distance
+        addSequential(new DrivetrainMoveInchesEncoderCommand(50, -0.8));
+        //TODO: Check angle
+        addSequential(new DrivetrainRotateAbsoluteDegreesPIDCommand(isRight? -135 : 135));
+        addParallel(new QuisitorOpenCommand());
+        //TODO: Check distance
+        addSequential(new DrivetrainMoveInchesEncoderCommand(10, 0.5));
+        addSequential(new QuisitorAcquireCommand());
+        addSequential(new QuisitorCloseCommand());
+        addSequential(new LiftMoveToHeightCommand(15));
+        addSequential(new DrivetrainMoveInchesEncoderCommand(6, 0.5));
+        addSequential(new DrivetrainRotateAbsoluteDegreesPIDCommand(180));
+        addSequential(new QuisitorDeacquireCommand());
+        addSequential(new DrivetrainMoveInchesEncoderCommand(6, -0.5));
+        addSequential(new LiftMoveToBottomCommand());
+        
+        //To acquire third cube and turn to scale
+        addSequential(new DrivetrainRotateAbsoluteDegreesPIDCommand(isRight? -135 : 135));
+        addParallel(new QuisitorOpenCommand());
+        addSequential(new DrivetrainMoveInchesEncoderCommand(10, 0.5));
+        addSequential(new QuisitorAcquireCommand());
+        addSequential(new QuisitorCloseCommand());
+        addSequential(new DrivetrainRotateAbsoluteDegreesPIDCommand(isRight? -90 : 90));
+    }
+}
