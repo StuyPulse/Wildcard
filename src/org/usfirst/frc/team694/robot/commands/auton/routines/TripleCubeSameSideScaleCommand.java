bd701f4c0db53b3b707a49040f8517dfa514f05d
Adris Jautakas <adrisj7@gmail.com>
2018-04-18 20:35:54 -0400

    Test three cube scale, got up to 2 cubes with minor tidbits (need timeouts)

--- src/org/usfirst/frc/team694/robot/commands/auton/routines/TripleCubeSameSideScaleCommand.java
+++ src/org/usfirst/frc/team694/robot/commands/auton/routines/TripleCubeSameSideScaleCommand.java
@@ -19,31 +19,32 @@ import edu.wpi.first.wpilibj.command.CommandGroup;
  * WITH CURVES
  */
 public class TripleCubeSameSideScaleCommand extends CommandGroup {
-    private static final double DISTANCE_TOTAL = 296 - 4;
+    private static final double DISTANCE_TOTAL = 130 + 116 + 40;//296 - 4;
     public TripleCubeSameSideScaleCommand(boolean isRight) {
-        
-        
+
         DrivetrainDriveCurveCommand driveCommand = new DrivetrainDriveCurveCommand(DISTANCE_TOTAL);
         driveCommand.addSpeedChange(0, 0.75);
-        driveCommand.addTurn(130, isRight ? -45 : 45);
-        driveCommand.addTurn(130 + 116, isRight ? 5 : -5);
-        
-        addParallel(new ConditionalDistanceEncodersCommand(new LiftMoveToHeightCommand(89 - RobotMap.MIN_HEIGHT_OF_LIFT - 12), 15));
+        driveCommand.addTurn(130 - 35, isRight ? -(45/2) : (45/2));
+//        driveCommand.addTurn(130 + 116, isRight ? 5 : -5);
+
+        addParallel(new ConditionalDistanceEncodersCommand(new LiftMoveToHeightCommand(89 - RobotMap.MIN_HEIGHT_OF_LIFT), 15));
         addSequential(driveCommand, 3.3);
         addSequential(new QuisitorMoveSpeedCommand(-0.7), 0.5);
+
+        addSequential(new DrivetrainMoveInchesEncoderCommand(-15, -0.4));
         addParallel(new LiftMoveToBottomCommand());
-        addSequential(new DrivetrainMoveInchesEncoderCommand(-15, 0.4));
         addSequential(new DrivetrainRotateAbsoluteDegreesPIDCommand(isRight ? -160 : 160));
         addSequential(new QuisitorOpenCommand());
         addParallel(new QuisitorAcquireCommand(), 2);
-        addSequential(new DriveStraightRampDownOnlyCommand(60));
+        addSequential(new DriveStraightRampDownOnlyCommand(60 - 10));
         addSequential(new QuisitorCloseCommand());
-        addParallel(new DrivetrainMoveInchesEncoderCommand(15, -0.4));
+        addParallel(new DrivetrainMoveInchesEncoderCommand(20, -0.4));
         addSequential(new DrivetrainRotateAbsoluteDegreesPIDCommand(0));
         addParallel(new LiftMoveToHeightCommand(68));
-        addSequential(new DrivetrainMoveInchesEncoderCommand(62, 0.5));
+        addSequential(new DrivetrainMoveInchesEncoderCommand(62 - 20, 0.5));
         addSequential(new QuisitorMoveSpeedCommand(-0.5), 1);
-        addParallel(new LiftMoveToBottomCommand());
+
+        addParallel(new ConditionalDistanceEncodersCommand(new LiftMoveToBottomCommand(), 15));
         addSequential(new DrivetrainMoveInchesEncoderCommand(32, -0.4));
         addParallel(new QuisitorOpenCommand());
         addSequential(new DrivetrainRotateAbsoluteDegreesPIDCommand(135));
