d46361c9bac264cb8d67747d98c967f936aa8f7a
adrisj7 <adrisj7@gmail.com>
2018-04-28 10:38:10 -0400

    Change initial drive distance and deacquire speed for 90 degree scale auto to prevent another collision with 1325

--- src/org/usfirst/frc/team694/robot/commands/auton/routines/SameSideScaleNinetyNullAutonCommand.java
+++ src/org/usfirst/frc/team694/robot/commands/auton/routines/SameSideScaleNinetyNullAutonCommand.java
@@ -3,6 +3,7 @@ package org.usfirst.frc.team694.robot.commands.auton.routines;
 import org.usfirst.frc.team694.robot.commands.LiftMoveToBottomCommand;
 import org.usfirst.frc.team694.robot.commands.LiftMoveToHeightCommand;
 import org.usfirst.frc.team694.robot.commands.QuisitorDeacquireCommand;
+import org.usfirst.frc.team694.robot.commands.QuisitorMoveSpeedCommand;
 import org.usfirst.frc.team694.robot.commands.auton.ConditionalDistanceEncodersCommand;
 import org.usfirst.frc.team694.robot.commands.auton.DriveStraightWithRampingCommand;
 import org.usfirst.frc.team694.robot.commands.auton.DrivetrainMoveInchesEncoderCommand;
@@ -22,7 +23,7 @@ public class SameSideScaleNinetyNullAutonCommand extends CommandGroup {
         addParallel(new ConditionalDistanceEncodersCommand(new LiftMoveToHeightCommand(5.0), 30));
         addParallel(new ConditionalDistanceEncodersCommand(new LiftMoveToHeightCommand(10.0), 250));
         //TODO: Change to the right distance for driving straight into the null zone 
-        addSequential(new DriveStraightWithRampingCommand(300), 4);
+        addSequential(new DriveStraightWithRampingCommand(300 + 5), 4);
         //TODO: Change the timeout of the Rotate Command
         addSequential(new DrivetrainRotateAbsoluteDegreesPIDCommand(isRight? -80 : 80), 2);
         //TODO: Check if the move height is correct
@@ -32,7 +33,7 @@ public class SameSideScaleNinetyNullAutonCommand extends CommandGroup {
         addSequential(new DrivetrainMoveInchesEncoderCommand(5, .3));
         //TODO: Check if this is necessary
         //addSequential(new DrivetrainMoveInchesEncoderCommand(5, 0.5), .5);
-        addSequential(new QuisitorDeacquireCommand(), 0.5);
+        addSequential(new QuisitorMoveSpeedCommand(-(1 - 0.5)), 0.5 + 0.5);
         addSequential(new DrivetrainMoveInchesEncoderCommand(5, -.3));
         //TODO: Check if this is necessary
 //        addSequential(new DrivetrainMoveInchesEncoderCommand(5, -0.5), .5);
